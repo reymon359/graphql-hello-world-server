eccf9619f3e4c439062929416d8c35a244705803
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-03-22 22:25:58 +0100

    feat(client/app.js): Update the title with the data fetched

--- client/app.js
+++ client/app.js
@@ -1,22 +1,25 @@
-const GRAPHQL_URL = 'http://localhost:9000/'
+const GRAPHQL_URL = 'http://localhost:9000/';
 
-async function fetchGreeting(){
-  const response = await fetch(GRAPHQL_URL, {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify({
-      query: `
+async function fetchGreeting() {
+	const response = await fetch(GRAPHQL_URL, {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json'
+		},
+		body: JSON.stringify({
+			query: `
         query {
           greeting
         }
       `
-    })
-  });
+		})
+	});
 
-  const responseBody = await response.json();
-  console.log(responseBody);
+	const { data } = await response.json();
+	return data;
 }
 
-fetchGreeting();
+fetchGreeting().then(({ greeting }) => {
+  const title = document.querySelector('h1');
+  title.textContent = greeting;
+});
